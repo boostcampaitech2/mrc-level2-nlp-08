7df7bdba35fea8cb5fc3ab334d8652c9a69fe3f3
l-yohai <yohan9612@naver.com>
2021-10-15 07:42:36 +0000

    fix: baseline

--- arguments.py
+++ arguments.py
@@ -9,7 +9,7 @@ class ModelArguments:
     """
 
     model_name_or_path: str = field(
-        default="klue/roberta-large",
+        default="klue/bert-base",
         metadata={
             "help": "Path to pretrained model or model identifier from huggingface.co/models"
         },
