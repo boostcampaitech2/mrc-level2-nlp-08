99527113c3515a9e9a5c51422e1e04666946926e
ddobokki <ddobokki@naver.com>
2021-10-23 16:12:20 +0000

    retrieval backup

--- retrieval_model.py
+++ retrieval_model.py
@@ -3,6 +3,8 @@ from transformers import (
     BertModel,
     ElectraModel,
     ElectraPreTrainedModel,
+    RobertaModel,
+    RobertaPreTrainedModel,
 )
 from torch import nn
 
@@ -50,4 +52,21 @@ class ElectraEncoder(ElectraPreTrainedModel):
         )
         sequence_output = outputs[0]
         pooled_output = self.pooler(sequence_output)
+        return pooled_output
+
+
+class RobertaEncoder(RobertaPreTrainedModel):
+    def __init__(self, config):
+        super(RobertaEncoder, self).__init__(config)
+
+        self.roberta = RobertaModel(config)
+        self.init_weights()
+
+    def forward(self, input_ids, attention_mask=None, token_type_ids=None):
+
+        outputs = self.roberta(
+            input_ids, attention_mask=attention_mask, token_type_ids=None
+        )
+
+        pooled_output = outputs[1]
         return pooled_output
--- retrieval_train.py
+++ retrieval_train.py
@@ -1,6 +1,10 @@
 import argparse
-from retrieval_model import BertEncoder, ElectraEncoder
-from utils import seed_everything
+from retrieval_model import BertEncoder, ElectraEncoder, RobertaEncoder
+from utils_mrc import (
+    get_tensor_for_dense_with_negative,
+    seed_everything,
+    get_tensor_for_dense,
+)
 from datasets import load_from_disk
 from transformers import (
     AutoTokenizer,
@@ -13,53 +17,32 @@ import torch
 import torch.nn.functional as F
 from torch.utils.data import DataLoader, RandomSampler, TensorDataset
 from tqdm import tqdm
+import wandb
 
 
 def to_cuda(batch):
     return tuple(t.cuda() for t in batch)
 
 
-def get_tensor_dataset(data_path: str, max_seq_length: int, tokenizer) -> TensorDataset:
-    """
-    question과 context를 tokenize 한 후 TensorDataset으로 concat하여 return합니다.
-    """
-    dataset = load_from_disk(data_path)
-    q_seqs = tokenizer(
-        dataset["question"],
-        max_length=max_seq_length,
-        padding="max_length",
-        truncation=True,
-        return_tensors="pt",
-    )
-    p_seqs = tokenizer(
-        dataset["context"],
-        max_length=max_seq_length,
-        padding="max_length",
-        truncation=True,
-        return_tensors="pt",
-    )
-    tensor_dataset = TensorDataset(
-        p_seqs["input_ids"],
-        p_seqs["attention_mask"],
-        p_seqs["token_type_ids"],
-        q_seqs["input_ids"],
-        q_seqs["attention_mask"],
-        q_seqs["token_type_ids"],
-    )
-
-    return tensor_dataset
-
-
 def train(args, p_encoder, q_encoder, train_dataset, valid_dataset):
+    wandb.login()
+    wandb.init(
+        project="retrieval_aug",
+        entity="chungye-mountain-sherpa",
+        name="retrieval_train",
+        group="klue-bert",
+    )
+
     # Dataloader
     train_sampler = RandomSampler(train_dataset)
     train_dataloader = DataLoader(
         train_dataset,
         sampler=train_sampler,
         batch_size=args.per_device_train_batch_size,
+        drop_last=True,
     )
     valid_dataloader = DataLoader(
-        valid_dataset, batch_size=args.per_device_train_batch_size, drop_last=False
+        valid_dataset, batch_size=args.per_device_eval_batch_size
     )
 
     # Optimizer
@@ -123,12 +106,16 @@ def train(args, p_encoder, q_encoder, train_dataset, valid_dataset):
 
     best_loss = 9999  # valid_loss
     num_epoch = 0
-    train_loss = 0
+
     for _ in range(int(args.num_train_epochs)):
         epoch_iterator = tqdm(train_dataloader, desc="Iteration")
         num_epoch += 1
 
+        train_loss = 0
+        train_acc = 0
+        train_step = 0
         for batch in epoch_iterator:
+            train_step += 1
             q_encoder.train()
             p_encoder.train()
 
@@ -160,9 +147,18 @@ def train(args, p_encoder, q_encoder, train_dataset, valid_dataset):
             if torch.cuda.is_available():
                 targets = targets.to("cuda")
 
+            # print(sim_scores)
+            # print()
+            # print(targets)
             sim_scores = F.log_softmax(sim_scores, dim=1)
             loss = F.nll_loss(sim_scores, targets)
             train_loss += loss.item()
+
+            _, preds = torch.max(sim_scores, 1)  #
+            train_acc += (
+                torch.sum(preds.cpu() == targets.cpu())
+                / args.per_device_train_batch_size
+            )
 
             loss.backward()
             optimizer.step()
@@ -171,66 +167,301 @@ def train(args, p_encoder, q_encoder, train_dataset, valid_dataset):
             p_encoder.zero_grad()
             global_step += 1
 
-            torch.cuda.empty_cache()
-        # validation
-        v_epoch_iterator = tqdm(valid_dataloader, desc="Iteration")
-        valid_loss = 0
-        for step, batch in enumerate(v_epoch_iterator):
-            with torch.no_grad():
-                q_encoder.eval()
-                p_encoder.eval()
+            # validation
+            if train_step % 100 == 0:
+                valid_loss = 0
+                valid_acc = 0
+                v_epoch_iterator = tqdm(valid_dataloader, desc="Iteration")
+                for step, batch in enumerate(v_epoch_iterator):
+                    with torch.no_grad():
+                        q_encoder.eval()
+                        p_encoder.eval()
 
-                if torch.cuda.is_available():
-                    batch = tuple(t.cuda() for t in batch)
-                p_inputs = {
-                    "input_ids": batch[0],
-                    "attention_mask": batch[1],
-                    "token_type_ids": batch[2],
-                }
-                q_inputs = {
-                    "input_ids": batch[3],
-                    "attention_mask": batch[4],
-                    "token_type_ids": batch[5],
-                }
-                p_outputs = p_encoder(**p_inputs)
-                q_outputs = q_encoder(**q_inputs)
+                        cur_batch_size = batch[0].size()[0]  # 마지막 배치 때문에
+                        if torch.cuda.is_available():
+                            batch = tuple(t.cuda() for t in batch)
+                        p_inputs = {
+                            "input_ids": batch[0],
+                            "attention_mask": batch[1],
+                            "token_type_ids": batch[2],
+                        }
+                        q_inputs = {
+                            "input_ids": batch[3],
+                            "attention_mask": batch[4],
+                            "token_type_ids": batch[5],
+                        }
+                        p_outputs = p_encoder(**p_inputs)
+                        q_outputs = q_encoder(**q_inputs)
 
-                sim_scores = torch.matmul(q_outputs, torch.transpose(p_outputs, 0, 1))
-                targets = torch.arange(0, args.per_device_train_batch_size).long()
-                if torch.cuda.is_available():
-                    targets = targets.to("cuda")
+                        sim_scores = torch.matmul(
+                            q_outputs, torch.transpose(p_outputs, 0, 1)
+                        )
+                        targets = torch.arange(0, cur_batch_size).long()
+                        if torch.cuda.is_available():
+                            targets = targets.to("cuda")
 
-                sim_scores = F.log_softmax(sim_scores, dim=1)
-                loss = F.nll_loss(sim_scores, targets)
+                        sim_scores = F.log_softmax(sim_scores, dim=1)
+                        loss = F.nll_loss(sim_scores, targets)
 
-                valid_loss += loss
+                        _, preds = torch.max(sim_scores, 1)  #
+                        valid_acc += (
+                            torch.sum(preds.cpu() == targets.cpu()) / cur_batch_size
+                        )
+
+                        valid_loss += loss
+                valid_loss = valid_loss / len(valid_dataloader)
+                valid_acc = valid_acc / len(valid_dataloader)
+                print()
+                print(f"valid loss: {valid_loss}")
+                print(f"valid acc: {valid_acc}")
+                wandb.log({"valid loss": valid_loss, "valid acc": valid_acc})
+                if best_loss > valid_loss:
+                    print("best model save")
+                    p_encoder.save_pretrained(args.output_dir + "/p_encoder")
+                    q_encoder.save_pretrained(args.output_dir + "/q_encoder")
+                    best_loss = valid_loss
 
         train_loss = train_loss / len(train_dataloader)
-        valid_loss = valid_loss / len(valid_dataloader)
+        train_acc = train_acc / len(train_dataloader)
+
         print(f"train loss: {train_loss}")
-        print(f"valid loss: {valid_loss}")
+        print(f"train acc: {train_acc}")
 
         # valid_loss가 작아질 때만 저장
         # 두 모델을 합쳐서 trainer에 넘겨줄 수 있게 만들면 좀더 간단해질듯
-        if best_loss > valid_loss:
-            p_encoder.save_pretrained(args.output_dir + "/p_encoder")
-            q_encoder.save_pretrained(args.output_dir + "/q_encoder")
-            best_loss = valid_loss
+    wandb.finish()
+
+    return p_encoder, q_encoder
+
+
+def train_with_negative(args, p_encoder, q_encoder, train_dataset, valid_dataset):
+    # Dataloader
+    train_sampler = RandomSampler(train_dataset)
+    train_dataloader = DataLoader(
+        train_dataset,
+        sampler=train_sampler,
+        batch_size=args.per_device_train_batch_size,
+        drop_last=True,
+    )
+    valid_dataloader = DataLoader(
+        valid_dataset, batch_size=args.per_device_eval_batch_size
+    )
+
+    # Optimizer
+    no_decay = ["bias", "LayerNorm.weight"]
+    optimizer_grouped_parameters = [
+        {
+            "params": [
+                p
+                for n, p in p_encoder.named_parameters()
+                if not any(nd in n for nd in no_decay)
+            ],
+            "weight_decay": args.weight_decay,
+        },
+        {
+            "params": [
+                p
+                for n, p in p_encoder.named_parameters()
+                if any(nd in n for nd in no_decay)
+            ],
+            "weight_decay": 0.0,
+        },
+        {
+            "params": [
+                p
+                for n, p in q_encoder.named_parameters()
+                if not any(nd in n for nd in no_decay)
+            ],
+            "weight_decay": args.weight_decay,
+        },
+        {
+            "params": [
+                p
+                for n, p in q_encoder.named_parameters()
+                if any(nd in n for nd in no_decay)
+            ],
+            "weight_decay": 0.0,
+        },
+    ]
+    optimizer = AdamW(
+        optimizer_grouped_parameters, lr=args.learning_rate, eps=args.adam_epsilon
+    )
+
+    t_total = (
+        len(train_dataloader)
+        // args.gradient_accumulation_steps
+        * args.num_train_epochs
+    )
+    scheduler = get_linear_schedule_with_warmup(
+        optimizer, num_warmup_steps=args.warmup_steps, num_training_steps=t_total
+    )
+
+    # Start training!
+    global_step = 0
+    if torch.cuda.is_available():
+        p_encoder.cuda()
+        q_encoder.cuda()
+
+    p_encoder.zero_grad()
+    q_encoder.zero_grad()
+    torch.cuda.empty_cache()
+
+    best_loss = 9999  # valid_loss
+    num_epoch = 0
+
+    for _ in range(int(args.num_train_epochs)):
+        epoch_iterator = tqdm(train_dataloader, desc="Iteration")
+        num_epoch += 1
+
+        train_loss = 0
+        train_acc = 0
+        train_step = 0
+        for batch in epoch_iterator:
+            train_step += 1
+            q_encoder.train()
+            p_encoder.train()
+
+            if torch.cuda.is_available():
+                batch = tuple(t.cuda() for t in batch)
+
+            p_inputs = {
+                "input_ids": torch.cat((batch[0], batch[3]), 0),
+                "attention_mask": torch.cat((batch[1], batch[4]), 0),
+                "token_type_ids": torch.cat((batch[2], batch[5]), 0),
+            }
+
+            q_inputs = {
+                "input_ids": batch[6],
+                "attention_mask": batch[7],
+                "token_type_ids": batch[8],
+            }
+
+            p_outputs = p_encoder(**p_inputs)  # (batch_size * 2, emb_dim)
+            q_outputs = q_encoder(**q_inputs)  # (batch_size, emb_dim)
+
+            # Calculate similarity score & loss
+            sim_scores = torch.matmul(
+                q_outputs, torch.transpose(p_outputs, 0, 1)
+            )  # (batch_size, emb_dim) x (emb_dim, batch_size) = (batch_size, batch_size * 2)
+
+            # target: position of positive samples = diagonal element
+            targets = torch.arange(0, args.per_device_train_batch_size).long()
+            if torch.cuda.is_available():
+                targets = targets.to("cuda")
+
+            # print(sim_scores)
+            # print()
+            # print(targets)
+            sim_scores = F.log_softmax(sim_scores, dim=1)
+            loss = F.nll_loss(sim_scores, targets)
+            train_loss += loss.item()
+
+            _, preds = torch.max(sim_scores, 1)  #
+            train_acc += (
+                torch.sum(preds.cpu() == targets.cpu())
+                / args.per_device_train_batch_size
+            )
+
+            loss.backward()
+            optimizer.step()
+            scheduler.step()
+            q_encoder.zero_grad()
+            p_encoder.zero_grad()
+            global_step += 1
+
+            # validation
+            if train_step % 50 == 0:
+                valid_loss = 0
+                valid_acc = 0
+                print(len(valid_dataset))
+                print(len(valid_dataloader))
+                v_epoch_iterator = tqdm(valid_dataloader, desc="Iteration")
+                for step, batch in enumerate(v_epoch_iterator):
+                    with torch.no_grad():
+                        q_encoder.eval()
+                        p_encoder.eval()
+
+                        cur_batch_size = batch[0].size()[0]  # 마지막 배치 때문에
+                        if torch.cuda.is_available():
+                            batch = tuple(t.cuda() for t in batch)
+                        p_inputs = {
+                            "input_ids": torch.cat((batch[0], batch[3]), 0),
+                            "attention_mask": torch.cat((batch[1], batch[4]), 0),
+                            "token_type_ids": torch.cat((batch[2], batch[5]), 0),
+                        }
+
+                        q_inputs = {
+                            "input_ids": batch[6],
+                            "attention_mask": batch[7],
+                            "token_type_ids": batch[8],
+                        }
+                        p_outputs = p_encoder(**p_inputs)
+                        q_outputs = q_encoder(**q_inputs)
+
+                        sim_scores = torch.matmul(
+                            q_outputs, torch.transpose(p_outputs, 0, 1)
+                        )
+                        targets = torch.arange(0, cur_batch_size).long()
+                        if torch.cuda.is_available():
+                            targets = targets.to("cuda")
+
+                        sim_scores = F.log_softmax(sim_scores, dim=1)
+                        loss = F.nll_loss(sim_scores, targets)
+
+                        _, preds = torch.max(sim_scores, 1)  #
+                        valid_acc += (
+                            torch.sum(preds.cpu() == targets.cpu()) / cur_batch_size
+                        )
+
+                        valid_loss += loss
+                valid_loss = valid_loss / len(valid_dataloader)
+                valid_acc = valid_acc / len(valid_dataloader)
+                print()
+                print(f"valid loss: {valid_loss}")
+                print(f"valid acc: {valid_acc}")
+                if best_loss > valid_loss:
+                    print("best model save")
+                    p_encoder.save_pretrained(args.output_dir + "/p_encoder")
+                    q_encoder.save_pretrained(args.output_dir + "/q_encoder")
+                    best_loss = valid_loss
+
+        train_loss = train_loss / len(train_dataloader)
+        train_acc = train_acc / len(train_dataloader)
+
+        print(f"train loss: {train_loss}")
+        print(f"train acc: {train_acc}")
+
+        # valid_loss가 작아질 때만 저장
+        # 두 모델을 합쳐서 trainer에 넘겨줄 수 있게 만들면 좀더 간단해질듯
 
     return p_encoder, q_encoder
 
 
 def main(args):
+    print(args)
+    seed_everything(args.seed)
     tokenizer = AutoTokenizer.from_pretrained(args.model_checkpoint)
-    train_dataset = get_tensor_dataset(
-        args.train_data_path, args.max_seq_length, tokenizer
+    train_dataset = get_tensor_for_dense(
+        data_path=args.train_data_path,
+        max_context_seq_length=args.max_context_seq_length,
+        max_question_seq_length=args.max_question_seq_length,
+        # num_neg=args.num_neg,
+        tokenizer=tokenizer,
     )
-    valid_dataset = get_tensor_dataset(
-        args.valid_data_path, args.max_seq_length, tokenizer
-    )
+    valid_dataset = get_tensor_for_dense_with_negative(
+        data_path=args.valid_data_path,
+        max_context_seq_length=args.max_context_seq_length,
+        max_question_seq_length=args.max_question_seq_length,
+        # num_neg=args.num_neg,
+        tokenizer=tokenizer,
+    )  # valid도 전처리 된걸 사용하려고 임시적으로 with_negative로 가져옴,
 
-    p_encoder = ElectraEncoder.from_pretrained(args.model_checkpoint)
-    q_encoder = ElectraEncoder.from_pretrained(args.model_checkpoint)
+    p_encoder = BertEncoder.from_pretrained(args.model_checkpoint)
+    q_encoder = BertEncoder.from_pretrained(args.model_checkpoint)
+    # p_encoder = ElectraEncoder.from_pretrained(args.model_checkpoint)
+    # q_encoder = ElectraEncoder.from_pretrained(args.model_checkpoint)
+    # p_encoder = RobertaEncoder.from_pretrained(args.model_checkpoint)
+    # q_encoder = RobertaEncoder.from_pretrained(args.model_checkpoint)
     """
     https://github.com/bcaitech1/p3-mrc-team-ikyo/blob/main/code/retrieval_model.py를 참고하면 별개의 Encoder를 하나로 통합 할 수 있을 듯
     변경 한다면 train 함수의 save_pretreained를 save_state_dict로 수정하여야함
@@ -247,11 +478,7 @@ def main(args):
     )
 
     p_encoder, q_encoder = train(
-        training_args,
-        p_encoder,
-        q_encoder,
-        train_dataset,
-        valid_dataset,
+        training_args, p_encoder, q_encoder, train_dataset, valid_dataset
     )
 
 
@@ -261,23 +488,30 @@ if __name__ == "__main__":
     parser.add_argument(
         "--model_checkpoint",
         type=str,
-        default="monologg/koelectra-base-v3-discriminator",
+        default="klue/bert-base",
     )
     parser.add_argument(
-        "--train_data_path", type=str, default="../data/train_dataset/train/"
+        "--train_data_path",
+        type=str,
+        default="/opt/ml/data/train_dataset/retrieval_train_add_wiki_qa",
     )
     parser.add_argument(
-        "--valid_data_path", type=str, default="../data/train_dataset/validation/"
+        "--valid_data_path",
+        type=str,
+        default="/opt/ml/data/train_dataset/elastic_retrieval_valid",
     )
-    parser.add_argument("--max_seq_length", type=int, default=512)
+    parser.add_argument("--max_context_seq_length", type=int, default=512)
+    parser.add_argument("--max_question_seq_length", type=int, default=30)
     parser.add_argument("--output_dir", type=str, default="retrieval")
     parser.add_argument("--evaluation_strategy", type=str, default="epoch")
     parser.add_argument("--learning_rate", type=float, default=5e-5)
-    parser.add_argument("--per_device_train_batch_size", type=int, default=16)
-    parser.add_argument("--per_device_eval_batch_size", type=int, default=16)
-    parser.add_argument("--num_train_epochs", type=int, default=10)
-    parser.add_argument("--weight_decay", type=float, default=0.01)
+    parser.add_argument("--per_device_train_batch_size", type=int, default=32)
+    parser.add_argument("--per_device_eval_batch_size", type=int, default=32)
+    parser.add_argument("--num_train_epochs", type=int, default=2)
+    parser.add_argument("--weight_decay", type=float, default=0.0)
+    parser.add_argument("--num_neg", type=int, default=2)
 
     args = parser.parse_args()
-
+    # "kykim/bert-kor-base"
+    # monologg/koelectra-base-v3-finetuned-korquad
     main(args=args)
