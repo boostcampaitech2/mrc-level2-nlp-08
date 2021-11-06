f691a1e285fcc7b7ae2d06d0e85cf506db07f6a2
ddobokki <ddobokki@naver.com>
2021-11-06 04:33:32 +0000

    add: hybrid retrieval

--- Retrieval/dense_train.py
+++ Retrieval/dense_train.py
@@ -2,18 +2,9 @@ import argparse
 
 from numpy.lib.function_base import gradient
 from dense_model import BertEncoder
-from utils_mrc import (
-    InBatchNegativeRandomDataset,
-    InBatchNegativeRandomDatasetNoTitle,
-    seed_everything,
-    get_tensor_for_dense,
-    get_tensor_for_dense_temp,
-    get_tensor_for_dense_negative,
-)
-from datasets import load_from_disk
+from dense_train_utils import InBatchNegativeRandomDatasetNoTitle, seed_everything
 from transformers import (
     AutoTokenizer,
-    AutoConfig,
     AdamW,
     TrainingArguments,
     get_linear_schedule_with_warmup,
@@ -28,216 +19,6 @@ import wandb
 
 def to_cuda(batch):
     return tuple(t.cuda() for t in batch)
-
-
-def train(args, p_encoder, q_encoder, train_dataset, valid_dataset):
-    wandb.login()
-    wandb.init(
-        project="retrieval_aug",
-        entity="chungye-mountain-sherpa",
-        name="retrieval_train",
-        group="klue-bert",
-    )
-
-    # Dataloader
-    train_sampler = RandomSampler(train_dataset)
-    train_dataloader = DataLoader(
-        train_dataset,
-        sampler=train_sampler,
-        batch_size=args.per_device_train_batch_size,
-        drop_last=True,
-    )
-    valid_dataloader = DataLoader(
-        valid_dataset, batch_size=args.per_device_eval_batch_size
-    )
-
-    # Optimizer
-    no_decay = ["bias", "LayerNorm.weight"]
-    optimizer_grouped_parameters = [
-        {
-            "params": [
-                p
-                for n, p in p_encoder.named_parameters()
-                if not any(nd in n for nd in no_decay)
-            ],
-            "weight_decay": args.weight_decay,
-        },
-        {
-            "params": [
-                p
-                for n, p in p_encoder.named_parameters()
-                if any(nd in n for nd in no_decay)
-            ],
-            "weight_decay": 0.0,
-        },
-        {
-            "params": [
-                p
-                for n, p in q_encoder.named_parameters()
-                if not any(nd in n for nd in no_decay)
-            ],
-            "weight_decay": args.weight_decay,
-        },
-        {
-            "params": [
-                p
-                for n, p in q_encoder.named_parameters()
-                if any(nd in n for nd in no_decay)
-            ],
-            "weight_decay": 0.0,
-        },
-    ]
-    optimizer = AdamW(
-        optimizer_grouped_parameters, lr=args.learning_rate, eps=args.adam_epsilon
-    )
-
-    t_total = (
-        len(train_dataloader)
-        // args.gradient_accumulation_steps
-        * args.num_train_epochs
-    )
-    scheduler = get_linear_schedule_with_warmup(
-        optimizer, num_warmup_steps=args.warmup_steps, num_training_steps=t_total
-    )
-
-    # Start training!
-    global_step = 0
-    if torch.cuda.is_available():
-        p_encoder.cuda()
-        q_encoder.cuda()
-
-    p_encoder.zero_grad()
-    q_encoder.zero_grad()
-    torch.cuda.empty_cache()
-
-    best_loss = 9999  # valid_loss
-    num_epoch = 0
-
-    for _ in range(int(args.num_train_epochs)):
-        epoch_iterator = tqdm(train_dataloader, desc="Iteration")
-        num_epoch += 1
-
-        train_loss = 0
-        train_acc = 0
-        train_step = 0
-        for batch in epoch_iterator:
-            train_step += 1
-            q_encoder.train()
-            p_encoder.train()
-
-            if torch.cuda.is_available():
-                batch = tuple(t.cuda() for t in batch)
-
-            p_inputs = {
-                "input_ids": batch[0],
-                "attention_mask": batch[1],
-                "token_type_ids": batch[2],
-            }
-
-            q_inputs = {
-                "input_ids": batch[3],
-                "attention_mask": batch[4],
-                "token_type_ids": batch[5],
-            }
-
-            p_outputs = p_encoder(**p_inputs)  # (batch_size, emb_dim)
-            q_outputs = q_encoder(**q_inputs)  # (batch_size, emb_dim)
-
-            # Calculate similarity score & loss
-            sim_scores = torch.matmul(
-                q_outputs, torch.transpose(p_outputs, 0, 1)
-            )  # (batch_size, emb_dim) x (emb_dim, batch_size) = (batch_size, batch_size)
-
-            # target: position of positive samples = diagonal element
-            targets = torch.arange(0, args.per_device_train_batch_size).long()
-            if torch.cuda.is_available():
-                targets = targets.to("cuda")
-
-            # print(sim_scores)
-            # print()
-            # print(targets)
-            sim_scores = F.log_softmax(sim_scores, dim=1)
-            loss = F.nll_loss(sim_scores, targets)
-            train_loss += loss.item()
-
-            _, preds = torch.max(sim_scores, 1)  #
-            train_acc += (
-                torch.sum(preds.cpu() == targets.cpu())
-                / args.per_device_train_batch_size
-            )
-
-            loss.backward()
-            optimizer.step()
-            scheduler.step()
-            q_encoder.zero_grad()
-            p_encoder.zero_grad()
-            global_step += 1
-
-            # validation
-            if train_step % 100 == 0:
-                valid_loss = 0
-                valid_acc = 0
-                v_epoch_iterator = tqdm(valid_dataloader, desc="Iteration")
-                for step, batch in enumerate(v_epoch_iterator):
-                    with torch.no_grad():
-                        q_encoder.eval()
-                        p_encoder.eval()
-
-                        cur_batch_size = batch[0].size()[0]  # 마지막 배치 때문에
-                        if torch.cuda.is_available():
-                            batch = tuple(t.cuda() for t in batch)
-                        p_inputs = {
-                            "input_ids": batch[0],
-                            "attention_mask": batch[1],
-                            "token_type_ids": batch[2],
-                        }
-                        q_inputs = {
-                            "input_ids": batch[3],
-                            "attention_mask": batch[4],
-                            "token_type_ids": batch[5],
-                        }
-                        p_outputs = p_encoder(**p_inputs)
-                        q_outputs = q_encoder(**q_inputs)
-
-                        sim_scores = torch.matmul(
-                            q_outputs, torch.transpose(p_outputs, 0, 1)
-                        )
-                        targets = torch.arange(0, cur_batch_size).long()
-                        if torch.cuda.is_available():
-                            targets = targets.to("cuda")
-
-                        sim_scores = F.log_softmax(sim_scores, dim=1)
-                        loss = F.nll_loss(sim_scores, targets)
-
-                        _, preds = torch.max(sim_scores, 1)  #
-                        valid_acc += (
-                            torch.sum(preds.cpu() == targets.cpu()) / cur_batch_size
-                        )
-
-                        valid_loss += loss
-                valid_loss = valid_loss / len(valid_dataloader)
-                valid_acc = valid_acc / len(valid_dataloader)
-                print()
-                print(f"valid loss: {valid_loss}")
-                print(f"valid acc: {valid_acc}")
-                wandb.log({"valid loss": valid_loss, "valid acc": valid_acc})
-                if best_loss > valid_loss:
-                    print("best model save")
-                    p_encoder.save_pretrained(args.output_dir + "/p_encoder")
-                    q_encoder.save_pretrained(args.output_dir + "/q_encoder")
-                    best_loss = valid_loss
-
-        train_loss = train_loss / len(train_dataloader)
-        train_acc = train_acc / len(train_dataloader)
-
-        print(f"train loss: {train_loss}")
-        print(f"train acc: {train_acc}")
-
-        # valid_loss가 작아질 때만 저장
-        # 두 모델을 합쳐서 trainer에 넘겨줄 수 있게 만들면 좀더 간단해질듯
-    wandb.finish()
-
-    return p_encoder, q_encoder
 
 
 def train_with_negative(
@@ -467,7 +248,6 @@ def train_with_negative(
         print(f"train acc: {train_acc}")
 
         # valid_loss가 작아질 때만 저장
-        # 두 모델을 합쳐서 trainer에 넘겨줄 수 있게 만들면 좀더 간단해질듯
     wandb.finish()
     return p_encoder, q_encoder
 
@@ -495,14 +275,6 @@ def main(args):
 
     p_encoder = BertEncoder.from_pretrained(args.model_checkpoint)
     q_encoder = BertEncoder.from_pretrained(args.model_checkpoint)
-    # p_encoder = ElectraEncoder.from_pretrained(args.model_checkpoint)
-    # q_encoder = ElectraEncoder.from_pretrained(args.model_checkpoint)
-    # p_encoder = RobertaEncoder.from_pretrained(args.model_checkpoint)
-    # q_encoder = RobertaEncoder.from_pretrained(args.model_checkpoint)
-    """
-    https://github.com/bcaitech1/p3-mrc-team-ikyo/blob/main/code/retrieval_model.py를 참고하면 별개의 Encoder를 하나로 통합 할 수 있을 듯
-    변경 한다면 train 함수의 save_pretreained를 save_state_dict로 수정하여야함
-    """
 
     training_args = TrainingArguments(
         output_dir=args.output_dir,
@@ -542,15 +314,17 @@ if __name__ == "__main__":
         "--train_bm25_path",
         type=str,
         default="/opt/ml/data/elastic_train_1000.bin",
-    )
+    )  # elastic search로 train query와 유사도가 높은 wiki의 문서를 dict로 저장한 경로 (query - top k wiki ids)
     parser.add_argument(
         "--valid_bm25_path",
         type=str,
         default="/opt/ml/data/elastic_valid_1000.bin",
-    )
+    )  # elastic search로 valid query와 유사도가 높은 wiki의 문서를 dict로 저장한 경로 (query - top k wiki ids)
     parser.add_argument("--max_context_seq_length", type=int, default=512)
     parser.add_argument("--max_question_seq_length", type=int, default=64)
-    parser.add_argument("--output_dir", type=str, default="retrieval")
+    parser.add_argument(
+        "--output_dir", type=str, default="/opt/ml/mrc-level2-nlp-08/Retrieval"
+    )
     parser.add_argument("--evaluation_strategy", type=str, default="epoch")
     parser.add_argument("--learning_rate", type=float, default=2e-5)
     parser.add_argument("--per_device_train_batch_size", type=int, default=16)
@@ -561,23 +335,4 @@ if __name__ == "__main__":
     parser.add_argument("--num_neg", type=int, default=50)
 
     args = parser.parse_args()
-    # "kykim/bert-kor-base"
-    # monologg/koelectra-base-v3-finetuned-korquad
-    # /opt/ml/data/train_dataset/train
-    # /opt/ml/data/train_dataset/retrieval_train_add_wiki_qa 위키 추가
-    # "monologg/kobigbird-bert-base"
     main(args=args)
-
-"""
-    parser.add_argument("--max_context_seq_length", type=int, default=512)
-    parser.add_argument("--max_question_seq_length", type=int, default=64)
-    parser.add_argument("--output_dir", type=str, default="retrieval")
-    parser.add_argument("--evaluation_strategy", type=str, default="epoch")
-    parser.add_argument("--learning_rate", type=float, default=2e-5)
-    parser.add_argument("--per_device_train_batch_size", type=int, default=16)
-    parser.add_argument("--per_device_eval_batch_size", type=int, default=16)
-    parser.add_argument("--gradient_accumulation_steps", type=int, default=1)
-    parser.add_argument("--num_train_epochs", type=int, default=50)
-    parser.add_argument("--weight_decay", type=float, default=0.01)
-    parser.add_argument("--num_neg", type=int, default=50)
-"""
--- Retrieval/retrieval.py
+++ Retrieval/retrieval.py
@@ -5,7 +5,7 @@ from elasticsearch import Elasticsearch, helpers
 from rank_bm25 import BM25Okapi
 from transformers import AutoTokenizer
 from tqdm import tqdm
-from dense_model import BertEncoder
+from .dense_model import BertEncoder
 import torch
 from torch.utils.data import DataLoader, TensorDataset, SequentialSampler
 import numpy as np
@@ -15,21 +15,27 @@ class Retrieval:
     def __init__(
         self,
         tokenizer,
-        data_path="../data/",
+        data_path="/opt/ml/mrc-level2-nlp-08/Retrieval/",
+        caching_path="caching/",
         context_path="/opt/ml/data/preprocess_wiki.json",
     ):
         self.tokenizer = tokenizer
         self.wiki_dataset = pd.read_json(context_path, orient="index")
+
+        caching_context_id_path = data_path + caching_path + "wiki_context_id_pair.bin"
+        caching_id_context_path = data_path + caching_path + "wiki_id_context_pair.bin"
+        caching_id_title_path = data_path + caching_path + "id_title_pair.bin"
+
         if (
-            os.path.isfile("wiki_context_id_pair.bin")
-            and os.path.isfile("wiki_id_context_pair.bin")
-            and os.path.isfile("id_title_pair.bin")
+            os.path.isfile(caching_context_id_path)
+            and os.path.isfile(caching_id_context_path)
+            and os.path.isfile(caching_id_title_path)
         ):
-            with open("wiki_context_id_pair.bin", "rb") as f:
+            with open(caching_context_id_path, "rb") as f:
                 self.wiki_context_id_dict = pickle.load(f)
-            with open("wiki_id_context_pair.bin", "rb") as f:
+            with open(caching_id_context_path, "rb") as f:
                 self.wiki_id_context_dict = pickle.load(f)
-            with open("id_title_pair.bin", "rb") as f:
+            with open(caching_id_title_path, "rb") as f:
                 self.wiki_id_title_dict = pickle.load(f)
         else:
             wiki_text = self.wiki_dataset["text"]
@@ -40,11 +46,11 @@ class Retrieval:
             self.wiki_id_context_dict = {k: v for k, v in zip(wiki_id, wiki_text)}
             self.wiki_id_title_dict = {k: v for k, v in zip(wiki_id, wiki_title)}
 
-            with open("wiki_context_id_pair.bin", "wb") as file:
+            with open(caching_context_id_path, "wb") as file:
                 pickle.dump(self.wiki_context_id_dict, file)
-            with open("wiki_id_context_pair.bin", "wb") as file:
+            with open(caching_id_context_path, "wb") as file:
                 pickle.dump(self.wiki_id_context_dict, file)
-            with open("id_title_pair.bin", "wb") as file:
+            with open(caching_id_title_path, "wb") as file:
                 pickle.dump(self.wiki_id_title_dict, file)
 
         self.wiki_corpus = list(self.wiki_context_id_dict.keys())
@@ -60,19 +66,26 @@ class SparseRetrieval(Retrieval):
     def __init__(
         self,
         tokenizer,
-        data_path="../data/",
+        data_path="/opt/ml/mrc-level2-nlp-08/Retrieval/",
+        caching_path="caching/",
         context_path="/opt/ml/data/preprocess_wiki.json",
     ):
-        super().__init__(tokenizer, data_path=data_path, context_path=context_path)
+        super().__init__(
+            tokenizer,
+            data_path=data_path,
+            caching_path=caching_path,
+            context_path=context_path,
+        )
         self.tokenized_corpus = [
             self.tokenizer.tokenize(context) for context in self.wiki_corpus
         ]
-        if os.path.isfile("BM25Okapi.bin"):
-            with open("BM25Okapi.bin", "rb") as f:
+        caching_bm25_path = data_path + caching_path + "BM25Okapi.bin"
+        if os.path.isfile(caching_bm25_path):
+            with open(caching_bm25_path, "rb") as f:
                 self.bm25 = pickle.load(f)
         else:
             self.bm25 = BM25Okapi(self.tokenized_corpus)
-            with open("BM25Okapi.bin", "wb") as f:
+            with open(caching_bm25_path, "wb") as f:
                 pickle.dump(self.bm25, f)
 
         self.es = Elasticsearch()
@@ -181,23 +194,31 @@ class DenseRetrieval(Retrieval):
         tokenizer,
         q_encoder_path,
         p_encoder_path,
-        data_path="../data/",
+        data_path="/opt/ml/mrc-level2-nlp-08/Retrieval/",
+        caching_path="caching/",
         context_path="/opt/ml/data/preprocess_wiki.json",
     ):
-        super().__init__(tokenizer, data_path=data_path, context_path=context_path)
+        super().__init__(
+            tokenizer,
+            data_path=data_path,
+            caching_path=caching_path,
+            context_path=context_path,
+        )
 
-        self.q_encoder = BertEncoder.from_pretrained(q_encoder_path)
-        self.p_encoder = BertEncoder.from_pretrained(p_encoder_path)
+        self.q_encoder = BertEncoder.from_pretrained(data_path + q_encoder_path)
+        self.p_encoder = BertEncoder.from_pretrained(data_path + p_encoder_path)
         if torch.cuda.is_available():
             self.p_encoder.cuda()
             self.q_encoder.cuda()
 
-        if os.path.isfile("dense_embedding.bin"):
-            with open("dense_embedding.bin", "rb") as f:
+        dense_embedding_path = data_path + caching_path + "dense_embedding.bin"
+
+        if os.path.isfile(dense_embedding_path):
+            with open(dense_embedding_path, "rb") as f:
                 self.p_embs = pickle.load(f)
         else:
             self.p_embs = self.get_wiki_dense_embedding(self.p_encoder)
-            with open("dense_embedding.bin", "wb") as f:
+            with open(dense_embedding_path, "wb") as f:
                 pickle.dump(self.p_embs, f)
 
     def get_wiki_dense_embedding(self, p_encoder):
@@ -324,3 +345,89 @@ class DenseRetrieval(Retrieval):
 
     def to_cuda(batch):
         return tuple(t.cuda() for t in batch)
+
+
+class HybridRetrieval(Retrieval):
+    def __init__(
+        self,
+        tokenizer,
+        q_encoder_path,
+        p_encoder_path,
+        data_path="/opt/ml/mrc-level2-nlp-08/Retrieval/",
+        caching_path="caching/",
+        context_path="/opt/ml/data/preprocess_wiki.json",
+    ):
+        super().__init__(
+            tokenizer,
+            data_path=data_path,
+            caching_path=caching_path,
+            context_path=context_path,
+        )
+        # # 테스트
+        # with open(data_path + caching_path + "es_q_ids.bin", "rb") as f:
+        #     self.es_ids = pickle.load(f)
+        # with open(data_path + caching_path + "es_q_scores.bin", "rb") as f:
+        #     self.es_score = pickle.load(f)
+        # # 테스트
+        # self.es_id = [43280, 47081, 35064, 24024, 42242]
+        # self.es_score = [20.487246, 20.10372, 19.851677, 19.529999, 18.778242]
+
+        self.sparse_retrieval = SparseRetrieval(tokenizer=tokenizer)
+        self.dense_retrieval = DenseRetrieval(
+            tokenizer=tokenizer,
+            p_encoder_path=p_encoder_path,
+            q_encoder_path=q_encoder_path,
+        )
+        self.q_encoder = self.dense_retrieval.q_encoder
+        self.p_embs = self.dense_retrieval.p_embs
+        if torch.cuda.is_available():
+            self.p_embs = torch.Tensor(self.p_embs).to("cuda")
+
+    def get_topk_doc_id_and_score(self, query, top_k):
+        es_id, es_score = self.sparse_retrieval.get_topk_doc_id_and_score(
+            query=query, top_k=top_k
+        )
+        return self.__rerank(query, es_id, es_score)
+
+    def get_topk_doc_id_and_score_for_querys(self, querys, top_k):
+        hybrid_ids = {}
+        hybrid_scores = {}
+
+        for i in tqdm(range(len(querys))):
+            query = querys[i]
+            doc_ids, scores = self.get_topk_doc_id_and_score(query, top_k)
+            hybrid_ids[query] = doc_ids
+            hybrid_scores[query] = scores
+
+        return hybrid_ids, hybrid_scores
+
+    def __rerank(self, query, es_id, es_score):
+        p_embs = self.p_embs
+        with torch.no_grad():
+            self.q_encoder.eval()
+            q_seqs_val = self.tokenizer(
+                query, padding="max_length", truncation=True, return_tensors="pt"
+            ).to("cuda")
+            q_emb = self.q_encoder(**q_seqs_val).to("cuda")
+        dot_prod_scores = torch.matmul(q_emb, torch.transpose(p_embs, 0, 1))
+        rank = torch.argsort(dot_prod_scores, dim=1, descending=True).squeeze()
+        rank = rank.cpu().numpy().tolist()
+
+        es_id_score = {k: v for k, v in zip(es_id, es_score)}
+
+        hybrid_id_score = dict()
+
+        for i in range(len(rank)):
+            dense_id = self.wiki_context_id_dict[self.wiki_corpus[i]]
+            if dense_id in es_id_score:
+                lin_score = dot_prod_scores[0][i].item() + es_id_score[dense_id]
+                hybrid_id_score[dense_id] = lin_score
+
+        hybrid_id_score = list(hybrid_id_score.items())
+        hybrid_id_score.sort(key=lambda x: x[1], reverse=True)
+        hybrid_ids = list(map(lambda x: x[0], hybrid_id_score))
+        hybrid_scores = list(map(lambda x: x[1], hybrid_id_score))
+        # hybrid_ids_dict[query] = hybrid_ids
+        # hybrid_socres_dict[query] = hybrid_scores
+
+        return hybrid_ids, hybrid_scores
